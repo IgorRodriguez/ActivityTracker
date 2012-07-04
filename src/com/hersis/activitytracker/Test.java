5b03ce9a8d3e8825e56d2d465fbe1f8494b78d12
igor <igor@ROOM>
2012-07-05 01:24:50 +0200

    Added Test class

---
+++ src/com/hersis/activitytracker/Test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hersis.activitytracker;
+
+/**
+ *
+ * @author Igor Rodriguez <igorrodriguezelvira@gmail.com>
+ */
+public class Test {
+	
+}
